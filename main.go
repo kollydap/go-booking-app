cebdd92ebabe62a343004ba1700d5f44e8b0b9bb
kollydap <dapkolly@gmail.com>
2024-02-13 23:27:46 +0100

    learnt maps

--- main.go
+++ main.go
@@ -1,113 +1,103 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"strconv"
 )
 
-type point struct {
-	x float32
-	y float32
-	z float32
-
-	func (p point) length() float64 {
-		return math.Sqrt(
-		(math.Pow(float64(p.x), 2) +
-		math.Pow(float64(p.y), 2) +
-		math.Pow(float64(p.z), 2)))
-		}
+type Contact struct {
+	email string
+	phone string
+	addr  string
 }
 
-
-func constructor(x, y, z float32) *point {
-	p := point{x: x, y: y, z: z}
-	return &p
-	}
-type Node struct{
-	next Node
-
+type Person struct {
+	name    string
+	age     int
+	contact Contact
 }
+
 func main() {
-	jultiArrayCreator()
-my_map:= make(map[string] int)
-my_map["kola"] = 89
 
-	// var name = "Kola booking"
-	// fmt.Printf("Welcome %v to booking app \n", name)
-
-	// var firstname string
-
-	// var age int
-	// num := 20
-	// num3 := 89
-	// final := num3 - num
-
-	// firstname = "Joseph"
-	// fmt.Printf("%T\n", age)
-	// fmt.Printf("welcome %v, you are %v years old", firstname, age)
-	// fmt.Printf("%v", final)
-
-	bookings := [6]string{"Nana", "Kola", "James", "Joseph"}
-	// // var anotherBook [7]string
-
-	// // var list []string
-	// // list = append(list, "james")
-	for i, booking := range bookings {
-		if booking == "Joseph" {
-			fmt.Println("HI Joseph")
-		}
-		fmt.Printf("%v\n %v\n ", i, booking)
-	}
 	i := 20
 	for i < 100 {
 		fmt.Printf("%v\n", i)
 		i = i + 1
 	}
-	city := "London"
 
-	switch city {
-	case "London":
-		fmt.Print(city)
-	case "Angeles":
-		fmt.Print(city)
-	default:
-		fmt.Printf("%v \n", city)
+	var person_map = make(map[int]Person)
+	person_map[1] = Person{
+		name: "kolawole",
+		age:  30,
+		contact: Contact{
+			email: "kollydap@gmail.com",
+			phone: "0590399303",
+			addr:  "number 3 shifao kola street",
+		},
+	}
+	person_map[2] = Person{
+		name: "kola",
+		age:  39,
+		contact: Contact{
+			email: "kola@gmail.com",
+			phone: "0590399303",
+			addr:  "number 3 shifao koladapao street",
+		},
+	}
+	new_person := map[int]Person{
+		1: Person{
+			name: "kola",
+			age:  39,
+			contact: Contact{
+				email: "kola@gmail.com",
+				phone: "0590399303",
+				addr:  "number 3 shifao koladapao street",
+			},
+		},
+		2: Person{
+			name: "kola",
+			age:  39,
+			contact: Contact{
+				email: "kola@gmail.com",
+				phone: "0590399303",
+				addr:  "number 3 shifao koladapao street",
+			},
+		},
+	}
+	for k, v := range new_person {
+		fmt.Printf("%v, %v \n", k, v)
 	}
 
-	greetUser()
-	if v, err := doSth(); !err {
-		//
-	} else {
-		fmt.Printf("%v", v)
-	}
 }
 
-func greetUser() {
-	var name string
-	fmt.Scanf("%s", &name)
-	age, _ := strconv.Atoi(name)
-	fmt.Printf("you entered %v", age)
+type User struct {
+	Firstname string
+	Age       int
 }
 
-func checker() bool {
-	num := 6
-	condition := num%2 == 0
-	return condition
-}
-
-func doSth() (int, bool) {
-	for pos, char := range "Hello, world!" {
-		fmt.Println("\n", pos, char)
-	}
-	return 20, false
-}
-
-func jultiArrayCreator() {
-	table := [5][6]string{}
-	for row := 0; row < 5; row++ {
-		for col := 0; col < 6; col++ {
-			table[row][col] = strconv.Itoa(row) + "," + strconv.Itoa(col)
+func jsonParser() {
+	var user User
+	var userlist [] User
+	json_string := `{
+		"firstname": "kolawole",
+		"age":40
+	}`
+	json_string = `[
+		{
+			"firstname": "kolawole",
+			"age":40
+		},
+		{
+			"firstname": "kolawole",
+			"age":40
 		}
+	]`
+
+	err := json.Unmarshal([]byte(json_string), &user)
+	if err != nil {
+		panic("ouch")
 	}
-	fmt.Printf("%v \n", table)
+	err = json.Unmarshal([]byte(json_string), &userlist)
+	
+
 }
